5506a7345140aa597bd885f9a8b4f6e0d5c2380c
muhammadnaeemmadni <muhammadnaeemmadni786@gmail.com>
2021-02-16 12:40:40 +0500

    done

---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCS_Lgkez_MGiZaYOF_S8oKBpNZhBfhcbc",
+  authDomain: "ecommerce-ec590.firebaseapp.com",
+  projectId: "ecommerce-ec590",
+  storageBucket: "ecommerce-ec590.appspot.com",
+  messagingSenderId: "363291596646",
+  appId: "1:363291596646:web:f584340c36156229b49aa7"
+  };
+
+
+export default firebaseConfig;
